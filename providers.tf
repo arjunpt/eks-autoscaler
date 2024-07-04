12b19ddde336ad4a49c57a22b38b4ff5c9806a3e
arjunpt <arjunpt2006@gmail.com>
2024-07-04 10:40:18 +0530

    remv0ed secrets

--- providers.tf
+++ providers.tf
@@ -1,8 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAXYKJW5IITFHPQSHI"
-  secret_key = "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"
 }
 
 terraform {
